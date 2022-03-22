63dc58cca00dd3fc0393408c89af077b56c2aee6
Emily Marigold Klassen <forivall@gmail.com>
2022-03-21 19:46:55 -0700

    test: hook up ci tests

--- photo-backend/jest.config.js
+++ photo-backend/jest.config.js
@@ -1,7 +1,15 @@
+/** @type {import('ts-jest').TsJestGlobalOptions} */
+const tsJestConfig = {
+  diagnostics: false,
+};
+
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
   preset: 'ts-jest',
   testMatch: ['<rootDir>/src/**/*.test.ts'],
   testPathIgnorePatterns: ['/node_modules/', '\\.d\\.ts$'],
+  globals: {
+    'ts-jest': tsJestConfig,
+  },
 };
 module.exports = config;
